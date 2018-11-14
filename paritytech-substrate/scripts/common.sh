cf7250cde14a464407c5f342fb38ed3535b9d456
The Ghost in the Machine <nitrogen1818@gmail.com>
2018-11-14 10:27:41 -0500

    fixed substrate initial commit

---
+++ paritytech-substrate/scripts/common.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+ROOT=`dirname "$0"`
+
+# A list of directories which contain wasm projects.
+SRCS=(
+	"core/executor/wasm"
+	"node/runtime/wasm"
+	"core/test-runtime/wasm"
+)
+
+# Make pushd/popd silent.
+
+pushd () {
+	command pushd "$@" > /dev/null
+}
+
+popd () {
+	command popd "$@" > /dev/null
+}
